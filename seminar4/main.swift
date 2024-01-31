15b117d73823c99b61e25326ebc79c8e3ead640d
AlexPer <alexper@list.ru>
2024-01-31 07:46:43 +0300

    small edited

--- seminar4/main.swift
+++ seminar4/main.swift
@@ -1,23 +1,6 @@
 //1. Создать перечисление с видами пиццы (хотя бы 4 - 5 кейсов)
-
-
-
 //2. Создать структуру пиццы, она должна содержать стоимость, вид пиццы, толстое или тонкое тесто и добавки (например, дополнительно добавить пепперони, помидоры, сыр). Вид пиццы должен быть вложенным типом для структуры пиццы. Подсказка: добавки лучше также сделать перечислением.
-
-
-
 struct Pizza {
-    enum Additives{
-        case cheeseMozzarella
-        case cheeseProvolone
-        case cheeseCheddar
-        case cheeseParmesan
-        case cheeseGouda
-        case tomatoes
-        case pepperoni
-        case salmon
-    }
-
     enum PizzaType{
         case pepperoni
         case cheese
@@ -33,14 +16,23 @@ struct Pizza {
         case chicagoDeepDish
         case greekStyle
     }
-
+    
+    enum Additives{
+        case cheeseMozzarella
+        case cheeseProvolone
+        case cheeseCheddar
+        case cheeseParmesan
+        case cheeseGouda
+        case tomatoes
+        case pepperoni
+        case salmon
+    }
     
     var price: Double
     var type: PizzaType
     var isThickDough: Bool
     var additives: Additives
 }
-
 
 //3. Создать класс пиццерии, добавить массив с возможными пиццами. Переменная с массивом должна быть приватной. Массив задается в инициализаторе.
 class Pizzeria{
@@ -67,6 +59,7 @@ let pizzaHawaiian = Pizza(price: 399.99, type: .hawaiian, isThickDough: true, ad
 var pizzaPepperoni = Pizza(price: 500, type: .hawaiian, isThickDough: false, additives: .cheeseGouda)
 var pizzaCheese = Pizza(price: 250.50, type: .cheese, isThickDough: true, additives: .tomatoes)
 var pizzaChicagoDeepDish = Pizza(price: 400, type: .chicagoDeepDish, isThickDough: false, additives: .salmon)
+
 myPizzeria.addPizza(pizza: pizzaHawaiian)
 myPizzeria.addPizza(pizza: pizzaPepperoni)
 myPizzeria.addPizza(pizza: pizzaCheese)
